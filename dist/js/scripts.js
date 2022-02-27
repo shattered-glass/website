7b70133232a252c3673848187d13945ca82f6b40
Christian Anderson <none>
2022-02-27 15:08:32 -0600

    Fixed Firebase Auth.

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,8 +1,3 @@
-/*!
-* Start Bootstrap - Scrolling Nav v5.0.4 (https://startbootstrap.com/template/scrolling-nav)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
-*/
 //
 // Scripts
 // 
